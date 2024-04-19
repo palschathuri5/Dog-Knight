a168c6cc61a82d5d79df7a820c6ba56686ffd125
palschathuri5 <palschathuri@gmail.com>
2024-04-18 21:30:46 -0500

    added box replace script and object

---
+++ Assets/Scripts/ItemReplace.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemReplace : MonoBehaviour
+{
+    public GameObject boxItem;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReplaceBox(){
+        Instantiate(boxItem, transform.position, transform.rotation); //replacing box crate with item, in same position/config
+        UnityEngine.Debug.Log("Replace item should succeed");
+        Destroy(gameObject);
+    }
+   
+}
